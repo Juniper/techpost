78149b401fd9b6bc9f59e905c35a7004deee7a78
door7302 <droy@hpe.com>
2026-08-07 18:45:30 +0200

    fix patch py script

--- .github/scripts/patch_mkdocs.py
+++ .github/scripts/patch_mkdocs.py
@@ -19,7 +19,14 @@ with open(MKDOCS_FILE, "r") as f:
 # -------------------------------
 # 1. Switch extra.css for branches
 # -------------------------------
-config["extra_css"] = "extra-branch.css"
+extra_css = config.get("extra_css", [])
+
+extra_css = [
+    "stylesheets/extra-branch.css" if css == "stylesheets/extra.css" else css
+    for css in extra_css
+]
+
+config["extra_css"] = extra_css
 
 
 # -------------------------------
