ea688ec7e53e774cf0f3ff74f99155e49e49cb8d
door7302 <droy@hpe.com>
2026-09-02 17:47:03 +0200

    fix main page

--- .github/scripts/patch_mkdocs.py
+++ .github/scripts/patch_mkdocs.py
@@ -63,7 +63,7 @@ index_md = f"""
 <div class="banner-section">
   <div class="banner-content">
     <p class="banner-eyebrow">HPE Networking Technical Library</p>
-    <p class="banner-title">Engineering insights, build for networks</p>
+    <p class="banner-title">Engineering insights, built for networks</p>
     <p class="banner-description">In-depth technical articles written by engineers, for engineers. Explore expert perspectives on data center, AI, routing, switching, security, wireless &amp; automation.</p>
     <a href="articles/menu/main/" class="banner-cta">View all articles</a>
   </div>
